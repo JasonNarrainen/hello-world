e4c59cc707193e2ab0fa35e80722c8a29567c6ce
Jason <jason.narrainen.1@etsmtl.net>
2018-03-13 17:21:27 -0400

    Added the LeetCode project with the Single Number class

---
+++ LeetCode/src/SingleNumber.java
@@ -0,0 +1,11 @@
+/*****************************************************************************
+ * Code for the Single Number exercise in Arrays in the easy collection from
+ * LeetCode.com that receives an int array with only duplicates except for one
+ * and you have to return that number that is unique in the array. 
+ * @author Jason Narrainen
+ * @version 13/03/2018
+ *
+ */
+public class SingleNumber {
+
+}
